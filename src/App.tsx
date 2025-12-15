e545ce7b9e12978f8c8578707585be07ec303b1d
kdy <kdy0814@g.shingu.ac.kr>
2025-12-15 10:48:26 +0900

    에러수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { ProjectDetail } from "./pages/ProjectDetail";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project/:id" element={<ProjectDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
